46a16be9e9c9040ef59553f6215220afe2e50001
BugYang <59379571+showarp@users.noreply.github.com>
2024-01-23 13:45:01 +0800

    Update val.py

--- val.py
+++ val.py
@@ -4,8 +4,8 @@ import json
 import os
 from torch.utils.tensorboard import SummaryWriter
 
-num_exp = len(os.listdir('./checkpoint/'))
-writer = SummaryWriter(f'./checkpoint/exp{num_exp-1}/log')
+num_exp = len(os.listdir('./checkpoint/'))-1
+writer = SummaryWriter(f'./checkpoint/exp{num_exp}/log')
 def val(net, epoch, loader, device):
     print("Evaluating")
     with open(f'./checkpoint/exp{num_exp}/info.json','r') as f:
