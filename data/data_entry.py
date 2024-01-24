6ae3746d900d56375d3a9f7a7d8bc52476815696
BugYang <59379571+showarp@users.noreply.github.com>
2024-01-24 15:01:54 +0800

    update new dataloader

--- data/data_entry.py
+++ data/data_entry.py
@@ -1,7 +1,9 @@
 from .tiny_genimage import tiny_genimage_dataloader
 from .tiny_sdv5 import tiny_sdv5_dataloader
+from .tiny_mj import tiny_mj_dataloader
 
 datas = {
     "tiny_genimage":tiny_genimage_dataloader,
     "tiny_sdv5":tiny_sdv5_dataloader,
+    "tiny_mj":tiny_mj_dataloader
 }
---
+++ data/tiny_mj.py
@@ -0,0 +1,58 @@
+import os
+from typing import Any
+from torch.utils.data import Dataset, DataLoader
+from .augment import *
+from PIL import Image
+
+
+class LoadData(Dataset):
+    def __init__(self, root, is_train=True, transforms = []) -> None:
+        super().__init__()
+        self.transforms = transforms
+        root = root
+        root_sdv5 = f"{root}/TinyGenImage/imagenet_midjourney"
+        if is_train==True:
+            root_paths = f"{root_sdv5}/train"
+        else:
+            root_paths = f"{root_sdv5}/val"
+
+        ai_file = f"{root_paths}/ai"
+        nature_file = f"{root_paths}/nature"
+        x_y = []
+        
+        for ai_img in os.listdir(ai_file):
+            x_y.append((f"{ai_file}/{ai_img}",1))
+        
+        for nature_img in os.listdir(nature_file):
+            x_y.append((f"{nature_file}/{nature_img}",0))
+        
+        self.x_y = x_y
+    
+    def __len__(self) -> int:
+        return len(self.x_y)
+    
+    def __getitem__(self, index) -> Any:
+        x,y = self.x_y[index]
+        x = Image.open(x).convert("RGB")
+        for transform in self.transforms:
+            x = transform(x)
+        return x,y
+
+def tiny_mj_dataloader(root, batch_size=32, num_workers=4):
+    """加载dataloader
+
+    Args:
+        batch_size (int, optional): batch size. Defaults to 32.
+        num_workers (int, optional): num workers. Defaults to 8.
+
+    Returns:
+        tupe: train_loader,val_loader
+    """
+    train_traisnforms = [public_transforms]
+    val_traisnforms = [public_transforms]
+
+    train_data = LoadData(root=root, is_train=True, transforms=train_traisnforms)
+    val_data = LoadData(root=root, is_train=False, transforms=val_traisnforms)
+    train_loader = DataLoader(train_data,batch_size, num_workers=num_workers, shuffle=True)
+    val_loader = DataLoader(val_data,batch_size, num_workers=num_workers, shuffle=True)
+    return train_loader,val_loader
