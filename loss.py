ccea8d02ef24c9f2187f2ef6df23adf6ca60c27a
showarp <exam6918@gmail.com>
2024-01-30 15:14:57 +0800

    修复klloss计算错误

--- loss.py
+++ loss.py
@@ -9,5 +9,29 @@ class SoftAugmentLoss:
         pass
 
     def __call__(self,predic,target) -> Any:
-        predic = F.log_softmax(predic,dim=1)
-        return F.kl_div(predic,target,reduction="batchmean")
+        predic = F.softmax(predic,dim=1)
+        return F.kl_div(predic.log(),target,reduction="batchmean")
+    
+class CKLoss:
+    def __init__(self) -> None:
+        pass
+    def __call__(self, predic, target) -> Any:
+        """CKLoss
+
+        Args:
+            predic (tensor): network output
+            target (tensor): soft label
+
+        Returns:
+            loss: tensor.float
+        """
+        predic_softmax = F.log_softmax(predic,dim=1)
+        kl_loss = F.kl_div(predic_softmax,target,reduction="batchmean")
+        ce_loss = F.cross_entropy(predic,target)
+        return kl_loss+ce_loss
+
+# x = torch.tensor([[1., 2., 3.]],dtype=torch.float32)
+# y = F.softmax(x,dim=1) 
+# x = F.softmax(x,dim=1).log()
+# # print(F.softmax(x).log(),F.log_softmax(x))
+# print(F.kl_div(x,y,reduction="batchmean"))
