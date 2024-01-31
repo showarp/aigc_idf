7d04565fbf20b2e82ada22ea357ee81e89172658
showarp <exam6918@gmail.com>
2024-01-31 21:44:52 +0800

    提交更多dataloader文件

--- data/data_entry.py
+++ data/data_entry.py
@@ -8,6 +8,12 @@ from .tiny_mj import tiny_mj_dataloader
 from .tiny_vqdm import tiny_vqdm_dataloader
 from .tiny_genimage_soft import tiny_genimage_soft_dataloader
 from .tiny_sdv5_soft import tiny_sdv5_soft_dataloader
+from .tiny_wukong_soft import tiny_wukong_soft_dataloader
+from .tiny_adm_soft import tiny_adm_soft_dataloader
+from .tiny_biggan_soft import tiny_biggan_soft_dataloader
+from .tiny_glide_soft import tiny_glide_soft_dataloader
+from .tiny_mj_soft import tiny_mj_soft_dataloader
+from .tiny_vqdm_soft import tiny_vqdm_soft_dataloader
 
 datas = {
     "tiny_genimage":tiny_genimage_dataloader,
@@ -19,5 +25,11 @@ datas = {
     "tiny_mj":tiny_mj_dataloader,
     "tiny_vqdm":tiny_vqdm_dataloader,
     "tiny_genimage_soft":tiny_genimage_soft_dataloader,
-    "tiny_sdv5_soft":tiny_sdv5_soft_dataloader
+    "tiny_sdv5_soft":tiny_sdv5_soft_dataloader,
+    "tiny_wukong_soft":tiny_wukong_soft_dataloader,
+    "tiny_adm_soft":tiny_adm_soft_dataloader,
+    "tiny_biggan_soft":tiny_biggan_soft_dataloader,
+    "tiny_glide_soft":tiny_glide_soft_dataloader,
+    "tiny_mj_soft":tiny_mj_soft_dataloader,
+    "tiny_vqdm_soft":tiny_vqdm_soft_dataloader,
 }
---
+++ data/tiny_adm_soft.py
@@ -0,0 +1,69 @@
+import os
+from typing import Any
+from torch.utils.data import Dataset, DataLoader
+from .augment import *
+from PIL import Image
+
+
+class LoadData(Dataset):
+    def __init__(self, root, is_train=True, transforms = []) -> None:
+        super().__init__()
+        self.transforms = transforms
+        root = root
+        root_sdv5 = f"{root}/TinyGenImage/imagenet_ai_0508_adm"
+        if is_train==True:
+            root_paths = f"{root_sdv5}/train"
+        else:
+            root_paths = f"{root_sdv5}/val"
+
+        ai_file = f"{root_paths}/ai"
+        nature_file = f"{root_paths}/nature"
+        x_y = []
+        
+        for ai_img in os.listdir(ai_file):
+            x_y.append((f"{ai_file}/{ai_img}",1))
+        
+        for nature_img in os.listdir(nature_file):
+            x_y.append((f"{nature_file}/{nature_img}",0))
+        
+        self.x_y = x_y
+    
+    def __len__(self) -> int:
+        return len(self.x_y)
+    
+    def __getitem__(self, index) -> Any:
+        x,y = self.x_y[index]
+        x = Image.open(x).convert("RGB")
+        img_quality = 1
+        n_classes = 2
+        for transform in self.transforms:
+            if transform == "blur":
+                img_quality = np.random.randint(30,101)*0.01
+                t = compose_blur_jpeg(img_quality,p=.5)
+                x = t(x)
+                continue
+            x = transform(x)
+        p = torch.tensor(1-(1-1/2)*(1-img_quality)**2)
+        soft_label = torch.zeros(2)
+        soft_label[::] = 1-p/(n_classes-1)
+        soft_label[y] = p
+        return x,soft_label
+
+def tiny_adm_soft_dataloader(root, batch_size=32, num_workers=4):
+    """加载dataloader
+
+    Args:
+        batch_size (int, optional): batch size. Defaults to 32.
+        num_workers (int, optional): num workers. Defaults to 8.
+
+    Returns:
+        tupe: train_loader,val_loader
+    """
+    train_traisnforms = [public_transforms0,"blur",public_transforms]
+    val_traisnforms = [public_transforms0,public_transforms]
+
+    train_data = LoadData(root=root, is_train=True, transforms=train_traisnforms)
+    val_data = LoadData(root=root, is_train=False, transforms=val_traisnforms)
+    train_loader = DataLoader(train_data,batch_size, num_workers=num_workers, shuffle=True)
+    val_loader = DataLoader(val_data,batch_size, num_workers=num_workers, shuffle=True)
+    return train_loader,val_loader
